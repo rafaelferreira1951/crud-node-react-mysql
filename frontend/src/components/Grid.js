a66a5769053233bca3b13513a0d7279497aab586
rafaelferreira1951 <rafaelferreira.1951@gmail.com>
2023-07-10 00:38:00 -0300

    commit

---
+++ frontend/src/components/Grid.js
@@ -0,0 +1,96 @@
+import React from "react";
+import styled from "styled-components";
+import { FaTrash, FaEdit } from "react-icons/fa";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const Table = styled.table`
+  width: 100%;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 5px;
+  max-width: 800px;
+  margin: 20px auto;
+  word-break: break-all;
+`;
+
+export const Thead = styled.thead``;
+
+export const Tr = styled.tr``;
+
+export const Tbody = styled.tbody``;
+
+export const Th = styled.th`
+  text-align: start;
+  border-bottom: inset;
+  padding-bottom: 5px;
+
+  @media (max-width: 500px) {
+    ${(props) => props.onlyWeb && "display: none"}
+  }
+`;
+
+export const Td = styled.td`
+  padding-top: 15px;
+  text-align: ${(props) => (props.alignCenter ? "center" : "start")};
+  width: ${(props) => (props.width ? props.width : "auto")};
+
+  @media (max-width: 500px) {
+    ${(props) => props.onlyWeb && "display: none"}
+  }
+`;
+
+const Grid = ({ users, setUsers, setOnEdit }) => {
+  const handleEdit = (id) => {
+    const user = users.find((item) => item.id === id);
+    setOnEdit(user);
+  };
+
+  const handleDelete = async (id) => {
+    await axios
+      .delete(`http://localhost:8800/${id}`)
+      .then(({ data }) => {
+        const newArray = users.filter((user) => user.id !== id);
+        setUsers(newArray);
+        toast.success(data);
+      })
+      .catch(({ response }) => {
+        toast.error(response.data);
+      });
+    setOnEdit(null);
+  };
+
+  return (
+    <Table>
+      <Thead>
+        <Tr>
+          <Th>Nome</Th>
+          <Th>Email</Th>
+          <Th onlyWeb>Fone</Th>
+          <Th></Th>
+          <Th></Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {users.map((item) => (
+          <Tr key={item.id}>
+            <Td width="30%">{item.nome}</Td>
+            <Td width="30%">{item.email}</Td>
+            <Td width="20%" onlyWeb>
+              {item.fone}
+            </Td>
+            <Td alignCenter width="5%">
+              <FaEdit onClick={() => handleEdit(item.id)} />
+            </Td>
+            <Td alignCenter width="5%">
+              <FaTrash onClick={() => handleDelete(item.id)} />
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  );
+};
+
+export default Grid;
